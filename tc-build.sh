0315688d156053c4cba5a3247efca7ec3eeb0467
GhostMaster69-dev <rathore6375@gmail.com>
2023-01-09 18:55:45 +0530

    tc-build: Build llvm all projects
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- tc-build.sh
+++ tc-build.sh
@@ -63,7 +63,7 @@ CC=clang CXX=clang++ CFLAGS=-O3 CXXFLAGS=-O3 ./build-llvm.py \
 	--shallow-clone \
 	--clang-vendor "$LLVM_NAME" \
 	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 LLVM_USE_LINKER=lld LLVM_ENABLE_LLD=ON" \
-	--projects "clang;lld;polly" \
+	--projects "all" \
 	--targets "ARM;AArch64;X86" \
 	--shallow-clone \
 	--quiet-cmake \
